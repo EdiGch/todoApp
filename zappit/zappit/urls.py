d21fb164115d9e8b0b0f5aa439d05dea0dda61e0
Grzegorz Charkiewicz <gcharkiewicz@enzode.com>
2022-01-03 11:20:22 +0100

    Adding a view to the application

--- zappit/zappit/urls.py
+++ zappit/zappit/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from posts import views
+from todoapp import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +24,6 @@ urlpatterns = [
     path('api/posts/<int:pk>', views.PostRetrieveDestroy.as_view()),
     path('api/posts/<int:pk>/vote', views.VoteCreate.as_view()),
     path('api-auth/', include('rest_framework.urls')),
+    # Todos
+    path('todoapp/', include('todoapp.urls'))
 ]
