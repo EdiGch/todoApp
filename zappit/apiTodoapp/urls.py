7b5726c540fe4586f4865b66317cb95e09d4aded
Grzegorz Charkiewicz <gcharkiewicz@enzode.com>
2022-01-04 15:40:21 +0100

    Create, Retrieve, Update, Destroy

--- zappit/apiTodoapp/urls.py
+++ zappit/apiTodoapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('list-create/', views.ListCreate.as_view()),
+    path('<int:pk>', views.RetrieveUpdateDestroy.as_view()),
     path('completed/', views.CompletedList.as_view()),
 ]
--- zappit/apiTodoapp/views.py
+++ zappit/apiTodoapp/views.py
@@ -11,3 +11,24 @@ class CompletedList(generics.ListAPIView):
         user = self.request.user
         return Todo.objects.filter(user=user, datecompleted__isnull=False).order_by(
             '-datecompleted')
+
+
+class ListCreate(generics.ListCreateAPIView):
+    serializer_class = ToDoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user, datecompleted__isnull=True)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class RetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ToDoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user)
